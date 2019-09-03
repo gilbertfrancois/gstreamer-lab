d6efb6cc2e68dffe0b787b80ead80a3ed4544249
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2019-09-03 18:58:51 +0200

    Updates install script

--- install_linux.sh
+++ install_linux.sh
@@ -3,6 +3,8 @@
 echo "-- Installing gstreamer packages..."
 
 $ sudo apt install \
+    cmake \
+    awscli \
     libssl1.0.0 \
     libgstreamer1.0-0 \
     gstreamer1.0-tools \
@@ -12,3 +14,8 @@ $ sudo apt install \
     gstreamer1.0-libav \
     libgstrtspserver-1.0-0 \
     libjansson4=2.11-1
+
+echo "-- Downloading test data"
+
+aws --profile ava-x-skuld s3 sync --sse AES256 s3://apothecary.smbh.ch/gst-lab/models/ models/
+aws --profile ava-x-skuld s3 sync --sse AES256 s3://apothecary.smbh.ch/gst-lab/3rdparty/ 3rdparty/
